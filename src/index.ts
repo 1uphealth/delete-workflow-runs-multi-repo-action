f6035194afc38ec57b1eb265f176c9134443da9c
Matt Riordan <matt.riordan@1up.health>
2024-05-14 14:11:36 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -2,74 +2,10 @@ import { getInput } from "@actions/core";
 import { Octokit } from "@octokit/rest";
 
 const token = getInput("token");
-const organization = "1uphealth";
 const retain_days = Number(getInput("retain_days"));
 let repositoryArray = JSON.parse(getInput("repository_array"));
 const owner = "1uphealth";
-// async function runAnalysis() {
-//   async function fetchGraphQL(query, variables) {
-//     const response = await fetch("https://api.github.com/graphql", {
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         query,
-//         variables,
-//       }),
-//     });
-//     return response.json();
-//   }
 
-//   async function getActiveBranches() {
-//     const retentionPeriod = new Date(
-//       Date.now() - retain_days * 24 * 60 * 60 * 1000
-//     ).toISOString();
-//     const branchQuery = `
-//     query ($organization: String!, $afterCursor: String) {
-//       organization(login: $organization) {
-//         repositories(first: 100, after: $afterCursor) {
-//           pageInfo {
-//             hasNextPage
-//             endCursor
-//           }
-//           nodes {
-//             name
-//             refs(first: 100, refPrefix: "refs/heads/") {
-//               nodes {
-//                 name
-//                 target {
-//                   ... on Commit {
-//                     committedDate
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `;
-
-//     const branchData = await fetchGraphQL(branchQuery, { organization });
-//     return branchData.data.organization.repositories.nodes.flatMap((repo) =>
-//       repo.refs.nodes
-//         .filter(
-//           (branch) =>
-//             new Date(branch.target.committedDate) > new Date(retentionPeriod)
-//         )
-//         .map((branch) => ({
-//           repository: repo.name,
-//           branch: branch.name,
-//         }))
-//     );
-//   }
-
-//   const activeBranches = await getActiveBranches();
-//   const filteredRuns = await filterWorkflowRuns(activeBranches);
-//   console.log(filteredRuns);
-// }
 const octokit = new Octokit({
   auth: token,
 });
